e91b587d5e44c187f79cb88c0bfd59ef6b33ddbf
030831 <kdhc7777@naver.com>
2026-08-12 22:31:28 +0900

    feat(product): add product

---
+++ src/main/java/com/example/product/domain/Product.java
@@ -0,0 +1,14 @@
+package com.example.product.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Product {
+
+    private Long id;
+    private String name;
+    private int price;
+    private int stock;
+}
